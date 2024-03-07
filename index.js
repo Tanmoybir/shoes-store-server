1434e66d301dd22709c0911ea324a6cb010f194b
Tanmoy Bir <tanmoy7468@gmail.com>
2024-03-07 20:56:08 +0600

    sort by price asc and desc

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // MeddleWare
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 app.use(express.json())
 
 
@@ -32,12 +35,27 @@ async function run() {
         app.get('/api/v1/shoes', async (req, res) => {
             try {
                 let query = {}
+                let sortObj = {}
+                const category = req.query.category
                 const company = req.query.company
-                // console.log(company);
+                const color = req.query.color
+                const sortField = req.query.sortField;
+                const sortOrder = req.query.sortOrder;
                 if(company){
                     query.company = company
                 }
-                const cursor = shoesCollection.find(query)
+                if(category) {
+                    query.category = category
+                }
+                if(color) {
+                    query.color = color
+                }
+
+                if (sortField && sortOrder) {
+                    sortObj[sortField] = sortOrder
+                }
+               
+                const cursor = shoesCollection.find(query).sort(sortObj)
                 const result = await cursor.toArray()
                 res.send(result)
             }
